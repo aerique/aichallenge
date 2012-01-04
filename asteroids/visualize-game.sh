b0e9f4a35ba8e1853fc0cacb43e0efb505afa15e
aerique <aerique@xs4all.nl>
2012-01-04 23:55:22 +0100

    Initial commit for Asteroids.

---
+++ asteroids/visualize-game.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BOT=0
+
+if [ "$1" != "" ]; then
+	BOT=$1
+fi
+
+rm -f tmp/*.png
+sbcl --script bin/bot-input2avi.sbcl game_logs/0.bot${BOT}.input
+ffmpeg -f image2 -i tmp/tmp-%04d.png tmp.mpg
